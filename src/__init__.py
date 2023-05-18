9b451722113d3c44d95009188a1568b004bbe709
Ana Paula R. Ferrari <88218648+Ana-Paula-Ferrari@users.noreply.github.com>
2023-05-18 10:36:02 -0300

    valores default

--- src/__init__.py
+++ src/__init__.py
@@ -75,7 +75,8 @@ app = create_app()
 
 if __name__ == "__main__":
     # Define a porta a ser usada pelo servidor Flask
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port, debug=True)
+   port = int(os.environ.get("PORT", 8080))
+   app.run(host='0.0.0.0', port=port, debug=True)
+
 
 CORS(app, resources={r"/api/*": {"origins": "*"}})
