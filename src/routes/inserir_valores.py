d2a785248d30a8b87dcf5ab621c34159fef6a7c8
victorramos887 <v.ramos58@hotmail.com>
2023-08-10 14:02:34 -0300

    inserir valores

--- src/routes/inserir_valores.py
+++ src/routes/inserir_valores.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, request, jsonify
-<<<<<<< HEAD
 from sqlalchemy import or_, and_
 from ..models import Escolas, Edificios, Hidrometros, TipoHidrometro, AreaUmida, Equipamentos, TiposEquipamentos, TipoDeAreaUmidaTipoDeEquipamento,db
 import pandas as pd
@@ -9,13 +8,6 @@ import math
 
 valores = Blueprint('valores', __name__, url_prefix='/api/v1/valores')
 
-=======
-from ..models import Escolas, db
-import pandas as pd
-
-
-valores = Blueprint('valores', __name__, url_prefix = '/api/v1/valores')
->>>>>>> ajustes
 
 @valores.post('/valor')
 def inserirGuarulhos():
@@ -25,7 +17,6 @@ def inserirGuarulhos():
 
     if escolas.filename == '':
         return "Nome de arquivo vazio", 400
-<<<<<<< HEAD
 
     try:
         dfEscola = pd.read_csv(escolas, header=0, sep=";", encoding='latin-1')
@@ -218,30 +209,3 @@ def inserirGuarulhos():
             "mensagem": "Erro não tratado!",
             "cod": str({e})
         }), 400
-=======
-    
-    try:
-        dfEscola = pd.read_csv(escolas, header=0, sep=";", encoding='latin-1')
-        dfAreaUmida = pd.read_csv(areaumida, header=0, sep=";", encoding='latin-1')
-
-        print(dfEscola.columns)
-
-        for index, row in dfEscola.iterrows():
-        
-            nome = row['Unidade Escolar'].upper()
-            escolasinsert = Escolas(nome=nome)
-
-            db.session.add(escolasinsert)
-
-        db.session.commit()
-            
-    except Exception as e:
-        return jsonify({
-            "mensagem":"Erro não tratado!",
-            "cod":str({e})
-        })
-
-    return jsonify({
-        "mensagem":"Valore retornado!"
-    })
->>>>>>> ajustes
