892cd6aec663647bdf70cc3f3fb286785afce8dc
Ana Paula R. Ferrari <88218648+Ana-Paula-Ferrari@users.noreply.github.com>
2023-09-18 16:18:49 -0300

    atualizacao try - exception nas rotas de cadastro

--- src/routes/cadastros.py
+++ src/routes/cadastros.py
@@ -16,23 +16,32 @@ cadastros = Blueprint('cadastros', __name__, url_prefix = '/api/v1/cadastros')
 @cadastros.post('/cliente')
 def cliente():
 
- try:
+    try:
 
-    formulario = request.get_json()
-    cliente = Cliente(**formulario)
-    db.session.add(cliente)
-    db.session.commit()
+        formulario = request.get_json()
+    
+    except Exception as e :
+        return jsonify({
+             "mensagem": "Não foi possível recuperar o formulario!",
+            "status": False,
+            "codigo": e
+        }), 400
         
-    return jsonify({'status':True, "mensagem":"Cadastro Realizado","data":cliente.to_json()}), HTTP_200_OK
-   
- except ArgumentError as e:
+    try:
+        cliente = Cliente(**formulario)
+        db.session.add(cliente)
+        db.session.commit()
+            
+        return jsonify({'status':True, "mensagem":"Cadastro Realizado","data":cliente.to_json()}), HTTP_200_OK
+    
+    except ArgumentError as e:
         error_message = str(e)
         error_data = {'error': error_message}
         json_error = json.dumps(error_data)
         print(json_error)
         return json_error
     
- except exc.DBAPIError as e:
+    except exc.DBAPIError as e:
         db.session.rollback()
         if e.orig.pgcode == '23505':
             # extrai o nome do campo da mensagem de erro
@@ -45,7 +54,7 @@ def cliente():
             return jsonify({'status': False, 'mensagem': 'Erro no cabeçalho.', 'codigo': str(e)}), HTTP_506_VARIANT_ALSO_NEGOTIATES
         return jsonify({'status': False, 'mensagem': 'Erro postgresql', 'codigo': str(e)}), 500
 
- except Exception as e:
+    except Exception as e:
         db.session.rollback()
         if isinstance(e, HTTPException) and e.code == '500':
             return jsonify({'status': False, 'mensagem': 'Erro interno do servidor', 'codigo': str(e)}), HTTP_500_INTERNAL_SERVER_ERROR
@@ -64,12 +73,28 @@ def usuario():
 
     try:
         formulario = request.get_json()
+        
+    except Exception as e :
+        return jsonify({
+             "mensagem": "Não foi possível recuperar o formulario!",
+            "status": False,
+            "codigo": e
+        }), 400
+    
+    try:
         cod_cliente = formulario['cod_cliente']
         nome = formulario['nome']
         email = formulario['email']
         senha = formulario['senha']
+    
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Verifique os nomes das variaveis do json enviado!!!",
+            "status": False
+        }), 400
         
 
+    try:
         #COLOCANDO LIMITE NA SENHA
         if len(senha) < 6:
             return jsonify({'error':'Senha muito curta'}), HTTP_400_BAD_REQUEST
@@ -125,8 +150,14 @@ def usuario():
 #Cadastros das escolas
 @cadastros.post('/escolas')
 def escolas():
-
-    formulario = request.get_json()
+    try:
+        formulario = request.get_json()
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Não foi possível recuperar o formulario!",
+            "status": False,
+            "codigo": e
+        }), 400
 
     try:
         nome = formulario["nome"]
@@ -142,6 +173,13 @@ def escolas():
         estado = formulario["estado"]
         bairro = formulario["bairro"]
         
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Verifique os nomes das variaveis do json enviado!!!",
+            "status": False
+        }), 400
+        
+    try:
         escola = Escolas(
             nome=nome,
             cnpj=cnpj,
@@ -216,12 +254,26 @@ def escolas():
 @cadastros.post('/reservatorios')
 def reservatorios():
 
-    formulario = request.get_json()
+    try:
+        formulario = request.get_json()
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Não foi possível recuperar o formulario!",
+            "status": False,
+            "codigo": e
+        }), 400
+        
     try:
        
         fk_escola = formulario['fk_escola']
         nome_do_reservatorio = formulario['nome']
-
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Verifique os nomes das variaveis do json enviado!!!",
+            "status": False
+        }), 400
+    
+    try:
         #CRIANDO O RESERVATORIO
         reservatorio = Reservatorios(
         fk_escola = fk_escola,
@@ -273,7 +325,14 @@ def edificios():
 
     try:
         formulario = request.get_json()
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Não foi possível recuperar o formulario!",
+            "status": False,
+            "codigo": e
+        }), 400
 
+    try:
         fk_escola = formulario['fk_escola']
         agua_de_reuso = formulario['agua_de_reuso']
         area_total_edificio = formulario['area_total_edificio'] if formulario['area_total_edificio'] != "" else 0.0
@@ -288,9 +347,13 @@ def edificios():
         numero_edificio = formulario['numero_edificio']
         pavimentos_edificio = formulario['pavimentos_edificio'] if formulario['pavimentos_edificio'] != "" else 0.0
         reservatorios = formulario['reservatorio']
-
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Verifique os nomes das variaveis do json enviado!!!",
+            "status": False
+        }), 400
         
-
+    try:
         edificio = Edificios(
             fk_escola=fk_escola,
             numero_edificio=numero_edificio,
@@ -308,8 +371,6 @@ def edificios():
         )
         
         db.session.add(edificio)
-
-        
 
 
         for reservatorio_enviado in reservatorios:
@@ -367,8 +428,16 @@ def edificios():
 @cadastros.post('/hidrometros')
 def hidrometros():
 
-    formulario = request.get_json()
-
+    try:
+        formulario = request.get_json()
+        
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Não foi possível recuperar o formulario!",
+            "status": False,
+            "codigo": e
+        }), 400
+        
     try:
         hidrometros = Hidrometros(**formulario)
         db.session.add(hidrometros)
@@ -419,16 +488,31 @@ def hidrometros():
 
 @cadastros.post('/populacao')
 def populacao():
-
-    formulario = request.get_json()
-    nivel = db.session.query(AuxOpNiveis.id).filter_by(nivel=formulario['nivel']).scalar()
+    
+    try:
+        formulario = request.get_json()
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Não foi possível recuperar o formulario!",
+            "status": False,
+            "codigo": e
+        }), 400
 
     try:
-
+        
         alunos = formulario['alunos']
+        nivel = formulario['nivel']
+        periodo = formulario['periodo']
         funcionarios = formulario['funcionarios']
         fk_edificios = formulario['fk_edificios']
-
+        
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Verifique os nomes das variaveis do json enviado!!!",
+            "status": False
+        }), 400
+        
+    try:
         nivel = db.session.query(AuxOpNiveis.id).filter_by(nivel=formulario['nivel']).scalar()
         periodo = db.session.query(AuxPopulacaoPeriodo.id).filter_by(periodo=formulario['periodo']).scalar()
         
@@ -484,8 +568,15 @@ def populacao():
 @cadastros.post('/area-umida')
 def area_umida():
 
-    formulario = request.get_json()
-
+    try:
+        formulario = request.get_json()
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Não foi possível recuperar o formulario!",
+            "status": False,
+            "codigo": e
+        }), 400
+        
     try:
         fk_edificios = formulario['fk_edificios']
         tipo_area_umida = formulario['tipo_area_umida']
@@ -493,6 +584,13 @@ def area_umida():
         localizacao_area_umida = formulario['localizacao_area_umida']
         status_area_umida = formulario['status_area_umida']
         operacao_area_umida = formulario['operacao_area_umida']
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Verifique os nomes das variaveis do json enviado!!!",
+            "status": False
+        }), 400
+        
+    try:
         tipos = AuxTipoAreaUmida.query.filter_by(tipo=tipo_area_umida).first()
        
         if status_area_umida == "Aberto":
@@ -564,9 +662,15 @@ def area_umida():
 @cadastros.post('/equipamentos')
 def equipamentos():
 
-    formulario = request.get_json()
-
-    #CONTINUAR DAQUI
+    try:
+        formulario = request.get_json()
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Não foi possível recuperar o formulario!",
+            "status": False,
+            "codigo": e
+        }), 400
+        
     try:
         fk_area_umida = formulario['fk_area_umida']
         tipo_equipamento = formulario['tipo_equipamento']
@@ -574,6 +678,13 @@ def equipamentos():
         quantTotal = formulario['quantTotal']
         quantProblema = formulario['quantProblema']
         quantInutil = formulario['quantInutil']
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Verifique os nomes das variaveis do json enviado!!!",
+            "status": False
+        }), 400 
+        
+    try:
         tipo_equipamento = AuxTiposEquipamentos.query.filter_by(aparelho_sanitario=tipo_equipamento).first().id
 
         # descricao_equipamento = DescricaoEquipamentos.query.filter_by(descricao=descricao_equipamento).first().id
--- src/routes/eventos.py
+++ src/routes/eventos.py
@@ -10,94 +10,6 @@ from sqlalchemy.exc import ArgumentError
 eventos = Blueprint('eventos', __name__, url_prefix = '/api/v1/cadastro-evento')
 
 
-# @eventos.post('/tipo-evento')
-# def tipoevento():
-#     meses_dict = {
-#         "Janeiro": 1,
-#         "Fevereiro": 2,
-#         "Março": 3,
-#         "Abril": 4,
-#         "Maio": 5,
-#         "Junho": 6,
-#         "Julho": 7,
-#         "Agosto": 8,
-#         "Setembro": 9,
-#         "Outubro": 10,
-#         "Novembro": 11,
-#         "Dezembro": 12
-#     }
-
-#     periodicidade = {
-#             "Ocasional":False, 
-#             "Recorrente":True
-#         }
-    
-#     try:
-#         formulario = request.get_json()
-
-#         print(formulario)
-
-#         fk_cliente = formulario.get("fk_cliente")
-#         nome_do_tipo_de_evento = formulario.get("nome_do_evento")
-#         periodicidade = periodicidade.get(formulario.get('periodicidade')) if formulario.get('periodicidade') is not None else False
-
-#         print(periodicidade)
-
-#         dia = formulario.get("dataRecorrente") if formulario.get('dataRecorrente') and formulario.get("dataRecorrente") !="" else None
-#         mes = meses_dict.get(formulario.get('mesRecorrente')) if formulario.get('mesRecorrente') and formulario.get('mesRecorrente') != "" else None
-#         requer_acao = formulario.get('requerResposta', None) if formulario.get('requerResposta') is not None else False
-#         tempo = formulario.get('tolerancia') if formulario.get('tolerancia') else None
-#         unidade = formulario.get('unidade') if formulario.get('unidade') else None
-#         acao = formulario.get('ehResposta') if formulario.get('ehResposta') is not None else False
-
-#         tipo_evento = AuxTipoDeEventos(
-#             fk_cliente=fk_cliente,
-#             nome_do_tipo_de_evento=nome_do_tipo_de_evento,
-#             periodicidade = periodicidade,
-#             dia=dia,
-#             mes=mes,
-#             requer_acao=requer_acao,
-#             tempo=tempo,
-#             unidade=unidade,
-#             acao=acao
-#         )
-
-#         db.session.add(tipo_evento)
-#         db.session.commit()
-        
-#         return jsonify({'status':True, "mensagem":"Cadastro Realizado","data":tipo_evento.to_json()}), HTTP_200_OK
-
-#     except ArgumentError as e:
-#         error_message = str(e)
-#         error_data = {'error': error_message}
-#         json_error = json.dumps(error_data)
-#         print(json_error)
-#         return json_error
-
-#     except exc.DBAPIError as e:
-#         db.session.rollback()
-#         if e.orig.pgcode == '23505':
-#             # extrai o nome do campo da mensagem de erro
-#             match = re.search(r'Key \((.*?)\)=', str(e))
-#             campo = match.group(1) if match else 'campo desconhecido'
-#             mensagem = f"Já existe um registro com o valor informado no campo '{campo}'. Por favor, corrija o valor e tente novamente."
-#             return jsonify({'status': False, 'mensagem': mensagem, 'código': str(e)}), HTTP_401_UNAUTHORIZED
-#         if e.orig.pgcode == '01004':
-#             return jsonify({'status': False, 'mensagem': 'Erro no cabeçalho.', 'codigo': str(e)}), HTTP_506_VARIANT_ALSO_NEGOTIATES
-#         return jsonify({'status': False, 'mensagem': 'Erro postgresql', 'codigo': str(e)}), 500
-
-#     except Exception as e:
-#         db.session.rollback()
-#         if isinstance(e, HTTPException) and e.code == '500':
-#             return jsonify({'status': False, 'mensagem': 'Erro interno do servidor', 'codigo': str(e)}), HTTP_500_INTERNAL_SERVER_ERROR
-#         if isinstance(e, HTTPException) and e.code == '400':
-#             #flash("Erro, 4 não salva")
-#             return jsonify({'status':False, 'mensagem': 'Erro na requisição', 'codigo':str(e)}), HTTP_400_BAD_REQUEST
-#         return jsonify({
-#             "erro":e
-#         })
-
-
 #cadastro de evento
 @eventos.post('/eventos')
 def eventos_cadastro():
@@ -105,6 +17,14 @@ def eventos_cadastro():
     try:
 
         formulario = request.get_json()
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Não foi possível recuperar o formulario!",
+            "status": False,
+            "codigo": e
+        }), 400
+    
+    try:
 
         fk_tipo = formulario.get("fk_tipo", None)
         nome = formulario.get("nome", None)
@@ -114,7 +34,13 @@ def eventos_cadastro():
         tipo_de_local = formulario.get("tipo_de_local", None)
         observacao = formulario.get("observacao", None)
         color = formulario.get("color", None)
-       
+    except Exception as e:
+        return jsonify({
+            "mensagem": "Verifique os nomes das variaveis do json enviado!!!",
+            "status": False
+        }), 400
+    
+    try:   
         
         evento = Eventos(
             fk_tipo=fk_tipo,
