e6507a5f8734aafa8b8db8f487d60e2c6ecbf3e3
Ana Paula R. Ferrari <88218648+Ana-Paula-Ferrari@users.noreply.github.com>
2024-02-07 10:30:26 -0300

    ajuste

--- src/routes/alertas.py
+++ src/routes/alertas.py
@@ -192,7 +192,7 @@ def avisos_escolas():
                 
                 #embaralhar lista e limitar itens
                 random.shuffle(avisos)                                                                      
-                return jsonify(avisos[:4])
+                return jsonify(avisos[:3])
     
  
             
